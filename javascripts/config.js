d1eea217026281ed7666f479d44479e57d0e499a
bjjay <fris@vip.qq.com>
2014-03-25 10:22:22 +0800

    new themes

---
+++ javascripts/config.js
@@ -0,0 +1,8 @@
+var disqus_shortname = 'bjjay';			//Short Name of disqus, this parameter is for disqus comment box.
+var hostbase = 'http://bjjay.github.io';		//The absolute root address of your blog without '/' in the end.
+var githubname = 'bjjay';						//Your username of GitHub.
+var repos = 'bjjay.github.io';					//Name of repository that contains your blog.
+var sitetitle = "Ajay's note";						//Name of your blog.
+var rss = '';									//Rss address of your blog.
+var suffix = '';								//Suffix of post files, '.' should be included.
+var branch = '';								//Branch of your blog repository. 'master' is default if you left it blank.
